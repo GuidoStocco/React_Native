ec90d9ff182dcf685189274689eafe3ce473a5e1
unknown <guidostoccodev@gmail.com>
2024-07-28 09:52:05 -0300

    component style

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import {View, Text} from 'react-native';
 export default function App(){
   return(
     <View>
-      <Text>Olá Mundog</Text>
-      <Text>Meu primeiro app</Text>
+      <Text>Olá Mundo</Text>
+      <Text style={{color:'#ff0000', fontSize: 25}} >Meu primeiro app</Text>
     </View>
   );
 };
