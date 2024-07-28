310c33633f48ab59f8fa609ba7647ab2ee434523
unknown <guidostoccodev@gmail.com>
2024-07-28 10:17:13 -0300

    adicionando propriedades ajustaveis

--- App.js
+++ App.js
@@ -4,25 +4,26 @@ import {View, Text, Image} from 'react-native';
 export default function App(){
 
   let nome = 'Guido';
-  let img = 'http://sujeitoprogramador.com/reactlogo.png';
 
   return(
     <View>
       <Text>Olá Mundo</Text>
       <Text style={{color:'#ff0000', fontSize: 25}} >Meu primeiro app</Text>
 
-    <Image
-      source={{uri: img}}
-      style={{width:300, height:300}}
-    />
-
     <Text style={{fontSize:20}}>{nome}</Text>
 
+    <Logo largura={350} altura={200} /> 
     </View>
     
   );
 };
   
+function Logo(props){
+  let img = 'http://sujeitoprogramador.com/reactlogo.png';
+  return(
+    <Image source={{uri: img}} style={{width: props.largura, height: props.altura}}/>
+  );
+}
   
   
   
