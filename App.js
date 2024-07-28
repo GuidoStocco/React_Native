b7afad6c5a79636a84036e922a832131cf342611
unknown <guidostoccodev@gmail.com>
2024-07-28 10:00:46 -0300

    adicionando Image

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 export default function App(){
   return(
     <View>
       <Text>Olá Mundo</Text>
       <Text style={{color:'#ff0000', fontSize: 25}} >Meu primeiro app</Text>
+
+    <Image
+      source={{uri: 'http://sujeitoprogramador.com/reactlogo.png'}}
+      style={{width:300, height:300}}
+    />
+
     </View>
+    
   );
 };
   
