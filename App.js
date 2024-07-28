65b1824a63fb724b75b6c486e85ea9a24200029f
unknown <guidostoccodev@gmail.com>
2024-07-28 10:05:06 -0300

    testando variáveis em react native

--- App.js
+++ App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 
 export default function App(){
+
+  let nome = 'Guido';
+  let img = 'http://sujeitoprogramador.com/reactlogo.png';
+
   return(
     <View>
       <Text>Olá Mundo</Text>
       <Text style={{color:'#ff0000', fontSize: 25}} >Meu primeiro app</Text>
 
     <Image
-      source={{uri: 'http://sujeitoprogramador.com/reactlogo.png'}}
+      source={{uri: img}}
       style={{width:300, height:300}}
     />
+
+    <Text style={{fontSize:20}}>{nome}</Text>
 
     </View>
     
